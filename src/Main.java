6bbfa3410ae8aa232541fc95579093b0ec51d4a3
minseok128 <minseok128128@gmail.com>
2023-12-03 01:19:06 +0900

    initialize: part 1

--- src/Main.java
+++ src/Main.java
@@ -12,67 +12,82 @@ public class Main {
     static final String DB_URL = "jdbc:mysql://localhost/assignment13?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     static final String USER = "root";
     static final String PASS = "1234";
+    static Connection conn;
+    static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         // 데이터베이스 연결
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
-            // 쿼리문 정의
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
             String QUERY_INFO = "SELECT * FROM student WHERE ID = ?";
             String QUERY_TAKE = "SELECT T.course_id, C.title, T.sec_id, T.semester, T.year, T.grade " +
                     "FROM takes AS T, course AS C " +
                     "WHERE ID = ? AND T.course_id = C.course_id";
 
-            // PreparedStatement 생성
-            try (PreparedStatement pstmt1 = conn.prepareStatement(QUERY_INFO);
-                 PreparedStatement pstmt2 = conn.prepareStatement(QUERY_TAKE)) {
+            while (true) {
+                searchCourse();
+                System.out.println("\n");
+            }
 
-                // 무한 반복하여 사용자 입력 처리
-                while (true) {
-                    System.out.print("┌ 학번을 입력하세요 (종료하려면 'exit' 입력): ");
-                    String input = scanner.nextLine();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-                    // 'exit' 입력 시 종료
-                    if ("exit".equalsIgnoreCase(input)) {
-                        System.out.println("프로그램 종료.");
-                        break;
-                    }
+    static void searchCourse() {
+        String Query1 = """
+                SELECT course_id, title, credits
+                FROM course
+                WHERE course_id IN (
+                  SELECT S.course_id
+                  FROM section as S, course_dept as CD
+                  WHERE (S.year, S.semester) = (?, ?)
+                  ANd S.course_id = CD.course_id
+                  GROUP BY S.course_id
+                  HAVING COUNT(*) >= 2
+                )""";
+        String Query2 = """
+                SELECT dept_name
+                FROM course_dept
+                WHERE course_id = ?
+                """;
+        try {
+            PreparedStatement pstmt1 = conn.prepareStatement(Query1);
+            PreparedStatement pstmt2 = conn.prepareStatement(Query2);
 
-                    // 쿼리문에 파라미터 설정
-                    pstmt1.setString(1, input);
-                    pstmt2.setString(1, input);
+            System.out.print("┌ 연도와 학기를 입력하세요: ");
+            int year = sc.nextInt();
+            String semester = sc.next();
+            pstmt1.setInt(1, year);
+            pstmt1.setString(2, semester);
 
-                    // 학생 정보 조회 및 출력
-                    try (ResultSet rs = pstmt1.executeQuery()) {
-                        if (rs.next()) {
-                            System.out.print("└ 학번: " + rs.getString("ID"));
-                            System.out.print(" | 이름: " + rs.getString("name"));
-                            System.out.print(" | 학과: " + rs.getString("dept_name"));
-                            System.out.println(" | 총 수강학점: " + rs.getString("tot_cred"));
-                        } else {
-                            System.out.println("해당 학번의 학생을 찾을 수 없습니다.");
+            try (ResultSet rs1 = pstmt1.executeQuery()) {
+                while (rs1.next()) {
+                    String course_id = rs1.getString("course_id");
+                    System.out.print("└ course_id: " + course_id);
+                    System.out.print(" | title: " + rs1.getString("title"));
+                    System.out.print(" | credits: " + rs1.getString("credits"));
+
+                    System.out.print(" | dept_names: ");
+                    pstmt2.setString(1, course_id);
+                    try (ResultSet rs2 = pstmt2.executeQuery()) {
+                        while (rs2.next()) {
+                            System.out.print(rs2.getString("dept_name") + ", ");
                         }
                     }
-
-                    // 수강한 과목 정보 조회 및 출력
-                    try (ResultSet rs = pstmt2.executeQuery()) {
-                        System.out.println("┌ 과목번호, 과목명, 분반번호, 년도, 학기, 성적");
-                        while (rs.next()) {
-                            System.out.print("├ " + rs.getString("T.course_id"));
-                            System.out.print(", " + rs.getString("C.title"));
-                            System.out.print(", " + rs.getString("T.sec_id"));
-                            System.out.print(", " + rs.getString("T.year"));
-                            System.out.print(", " + rs.getString("T.semester"));
-                            System.out.println(", " + rs.getString("T.grade"));
-                        }
-                    }
-                    System.out.println("\n");
                 }
             }
 
         } catch (SQLException e) {
-            // 예외 처리
             e.printStackTrace();
         }
     }
