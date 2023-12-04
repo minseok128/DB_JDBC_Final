f83604973fde309f85d203f01c19a17aa5d59b0a
minseok128 <minseok128128@gmail.com>
2023-12-04 20:39:55 +0900

    initialize: part 2

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
     }
 
     static void searchCourse() {
-        String Query1 = """
+        String query1 = """
                 SELECT course_id, title, credits
                 FROM course
                 WHERE course_id IN (
@@ -44,14 +44,58 @@ public class Main {
                   GROUP BY S.course_id
                   HAVING COUNT(*) >= 2
                 )""";
-        String Query2 = """
+        String query2 = """
                 SELECT dept_name
                 FROM course_dept
                 WHERE course_id = ?
                 """;
         try {
-            PreparedStatement pstmt1 = conn.prepareStatement(Query1);
-            PreparedStatement pstmt2 = conn.prepareStatement(Query2);
+            PreparedStatement pstmt1 = conn.prepareStatement(query1);
+            PreparedStatement pstmt2 = conn.prepareStatement(query2);
+
+            System.out.print("┌ 연도와 학기를 입력하세요: ");
+            int year = sc.nextInt();
+            String semester = sc.next();
+            pstmt1.setInt(1, year);
+            pstmt1.setString(2, semester);
+
+            try (ResultSet rs1 = pstmt1.executeQuery()) {
+                while (rs1.next()) {
+                    String course_id = rs1.getString("course_id");
+                    System.out.print("└ course_id: " + course_id);
+                    System.out.print(" | title: " + rs1.getString("title"));
+                    System.out.print(" | credits: " + rs1.getString("credits"));
+
+                    System.out.print(" | dept_names: ");
+                    pstmt2.setString(1, course_id);
+                    try (ResultSet rs2 = pstmt2.executeQuery()) {
+                        while (rs2.next()) {
+                            System.out.print(rs2.getString("dept_name") + ", ");
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void sectionTimeAndClassroom() {
+        String query1 = """
+                SELECT  S.course_id, C.title, S.sec_id
+                FROM section as S, course as C
+                WHERE (S.year, S.semester) = (?, ?)
+                AND S.course_id = C.course_id
+                """;
+        String query2 = """
+                SELECT T.day, T.start_time, T.end_time, R.building, R.room_number
+                FROM rooms as R, time_slot as T
+                WHERE (R.time_slot_id, R.day, R.start_time) = (T.time_slot_id, T.day, T.start_time)
+                AND (R.course_id, R.sec_id, R.year, R.semester) = (?, ?, ?, ?)
+                """;
+        try {
+            PreparedStatement pstmt1 = conn.prepareStatement(query1);
+            PreparedStatement pstmt2 = conn.prepareStatement(query2);
 
             System.out.print("┌ 연도와 학기를 입력하세요: ");
             int year = sc.nextInt();
